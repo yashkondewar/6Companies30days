3f68b7c6750c082cc4ab041da9e651a39acf9bfe
yashkondewar <kondewaryash@gmail.com>
2023-01-04 23:27:25 +0530

    5th

--- Microsoft/3) Bulls and Cows.cpp
+++ Microsoft/3) Bulls and Cows.cpp
@@ -1,20 +1,25 @@
-class Solution {
+class Solution
+{
 public:
-    string getHint(string secret, string guess) {
+    string getHint(string secret, string guess)
+    {
         int bulls = 0;
-        int cows  = 0;
-        vector<int> s(10,0);
-        vector<int> g(10,0);
-        for(int i = 0; i  < secret.length(); i++){
-            if(secret[i] == guess[i])
-            bulls++;
-            else{
-                s[secret[i]-'0']++;
-                g[guess[i]-'0']++;
+        int cows = 0;
+        vector<int> s(10, 0);
+        vector<int> g(10, 0);
+        for (int i = 0; i < secret.length(); i++)
+        {
+            if (secret[i] == guess[i])
+                bulls++;
+            else
+            {
+                s[secret[i] - '0']++;
+                g[guess[i] - '0']++;
             }
         }
-        for(int i = 0; i < 10; i++){
-            cows+=min(s[i],g[i]);
+        for (int i = 0; i < 10; i++)
+        {
+            cows += min(s[i], g[i]);
         }
         string ans = "";
         ans += to_string(bulls);
